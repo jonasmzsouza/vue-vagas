29d0abb5ad949c7fbf0837ca5ee827fa0b0b9c05
Jonas Souza <jonasmzsouza@gmail.com>
2023-07-19 11:48:05 -0300

    update: add the mitt depedency and config main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+//import and instantiate the mitt package for global bus
+import mitt from "mitt";
+const emitter = mitt();
+
+//start vue instance based on component,
+const app = createApp(App);
+
+//configure the miit package instance as a global property
+app.config.globalProperties.emitter = emitter;
+
+//bind vue instance to app id html element
+app.mount("#app");
