521412b024b73059a47095cb24a40635ef3ff783
Jonas Souza <jonasmzsouza@gmail.com>
2023-07-19 11:36:04 -0300

    update: add vagas-favoritas-component | checkbox to baga-component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <vagas-favoritas-component />
     <topo-component @navegar="componente = $event" />
     <conteudo-component :conteudo="componente" />
   </div>
@@ -8,12 +9,14 @@
 <script>
 import TopoComponent from "@/components/layouts/TopoComponent.vue";
 import ConteudoComponent from "@/components/layouts/ConteudoComponent.vue";
+import VagasFavoritasComponent from '@/components/comuns/VagasFavoritasComponent.vue';
 
 export default {
   name: "App",
   components: {
     TopoComponent,
     ConteudoComponent,
+    VagasFavoritasComponent,
   },
   data: () => ({
     componente: "HomeComponent",
---
+++ src/components/comuns/VagasFavoritasComponent.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <div class="div-vagas-favoritas">
+      <button
+        class="btn btn-success"
+        type="button"
+        data-bs-toggle="offcanvas"
+        data-bs-target="#offcanvasRight"
+        aria-controls="offcanvasRight"
+      >
+        Vagas Favoritas
+      </button>
+    </div>
+    <div
+      class="offcanvas offcanvas-end"
+      tabindex="-1"
+      id="offcanvasRight"
+      aria-labelledby="offcanvasRightLabel"
+    >
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasRightLabel">
+          Vagas Favoritadas
+        </h5>
+        <button
+          type="button"
+          class="btn-close"
+          data-bs-dismiss="offcanvas"
+          aria-label="Close"
+        ></button>
+      </div>
+      <div class="offcanvas-body">...</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VagasFavoritasComponent",
+};
+</script>
+
+<style scoped>
+.div-vagas-favoritas {
+  position: absolute;
+  z-index: 1;
+  top: 70px;
+  right: 5px;
+}
+</style>
